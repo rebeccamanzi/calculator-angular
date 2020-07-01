de3e97d3f8bf869c214eb7f3b7127c939dc08b92
rebecca-ajl <rebeccamanzi@gmail.com>
2020-06-30 21:22:42 -0300

    create the app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ola-mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ola-mundo');
+    expect(app.title).toEqual('Olá Mundo!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ola-mundo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Olá Mundo!');
   });
 });
